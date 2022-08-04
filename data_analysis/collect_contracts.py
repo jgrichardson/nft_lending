9bb3f4a01ba5190ff2034aaffb1e465487fb22f5
miltiades-the-general <miltiadesgeneral@gmail.com>
2022-08-03 17:05:05 -0700

    Correlational analysis whale ratio

--- data_analysis/collect_contracts.py
+++ data_analysis/collect_contracts.py
@@ -25,13 +25,6 @@ def collect_addresses(key, collections_list):
         contract_addresses[i] = fetch_data_address(baseurl, key)
     return contract_addresses
 
-     # 06799a1e4792001aa9114f0012b9650ca28059a3
-    # 1dfe7ca09e99d10835bf73044a23b73fc20623df
-    # 81ae0be3a8044772d04f32398bac1e1b4b215aa8
-    # 4e1f41613c9084fdb9e34e11fae9412427480e56
-    # 7bd29408f11d2bfc23c34f18275bbf23bb716bc7
-    # c36442b4a4522e871399cd717abdd847ab11fe88
-
 def collect_top_100(key):
     collections = {}
     baseurl = f"https://api.rarify.tech/data/contracts/?page[limit]=70&sort=-insights.volume"
